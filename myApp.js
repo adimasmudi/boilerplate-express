fbf5dbddcf7078f4893533508f78f7b4eee05f75
adimasmudi <adimasmudi@gmail.com>
2022-06-24 22:32:52 +0700

    tadi lupa rutenya

--- myApp.js
+++ myApp.js
@@ -2,8 +2,8 @@ let express = require('express');
 let app = express();
 
 // console.log("Hello World")
-app.get(function(req, res){
-    res.send("Hello Express")
+app.get('/',function(req, res){
+    res.send('Hello Express')
 })
 
 
