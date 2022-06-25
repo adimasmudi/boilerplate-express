e6c85aa8b07e359cd07957ccea36ccef79a22d58
adimasmudi <adimasmudi@gmail.com>
2022-06-26 00:17:03 +0700

    membenarkan

--- myApp.js
+++ myApp.js
@@ -3,10 +3,11 @@ let app = express();
 
 // console.log("Hello World")
 app.get('/',function(req, res){
+    app.use(__dirname+'/public',express.static())
     res.sendFile(__dirname + '/views/index.html')
 })
 
-app.use(__dirname+'/public',express.static())
+
 
 
 
