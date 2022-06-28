2ee8d3d5b47d70b235c1df829b0e19e780064e35
adimasmudi <adimasmudi@gmail.com>
2022-06-28 23:37:52 +0700

    terakhir

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 require('dotenv').config()
 
-
+process.env.MESSAGE_STYLE='uppercase';
 
 // console.log("Hello World")
 app.get('/',function(req, res){
