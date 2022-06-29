0cfd0e650782c48025cb592a7bbf3e8332233c36
adimasmudi <adimasmudi@gmail.com>
2022-06-29 15:09:05 +0700

    add middleware

--- myApp.js
+++ myApp.js
@@ -4,13 +4,20 @@ require('dotenv').config()
 
 process.env.MESSAGE_STYLE='uppercase';
 
+function middleware(req, res, next){
+    console.log(req.method)
+    console.log(req.path)
+    console.log(req.ip)
+    next()
+}
+
 // console.log("Hello World")
 app.get('/',function(req, res){
     app.use('/public',express.static(__dirname+'/public'))
     res.sendFile(__dirname + '/views/index.html')
 })
 
-app.get('/json', (req, res) => {
+app.get('/json',middleware, (req, res) => {
     let message = 'Hello json';
     (process.env.MESSAGE_STYLE == 'uppercase') ? message=message.toUpperCase() : message=message; res.json({'message': message});
     });
